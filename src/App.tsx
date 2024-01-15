1caeb4f3c1b7fa0ec1eddb309d592ee0a96e2230
AlexandreBottet <alexandre.bottet@outlook.fr>
2024-01-15 15:08:47 +0100

    Add components to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import Header from './components/Header';
+import Filter from './components/Filter';
+import Games from './components/Games';
+import Figurines from './components/Figurines';
+
 import './App.css';
 
 const App = () => {
 
   return (
     <>
-      
+      <Header />
+      <Filter />
+      <Games />
+      <Figurines />
     </>
   );
 };
